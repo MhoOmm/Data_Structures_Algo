97252adae369e27dadc76411dcadd713ad223c2b
Mho <ommtripathi046@gmail.com>
2025-12-20 12:50:55 +0530

    delete-columns-to-make-sorted

---
+++ Easy/delete-columns-to-make-sorted.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int time  = strs.size();
+        int n = strs[0].size();
+        int count =0;
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<time;j++)
+            {
+                if(strs[j][i]<strs[j-1][i])
+                {
+                    count++;
+                    break;
+                }
+
+            }
+        }
+
+        return count;
+    }
+};
