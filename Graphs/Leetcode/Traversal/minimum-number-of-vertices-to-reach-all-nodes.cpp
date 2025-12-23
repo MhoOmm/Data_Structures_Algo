47b386adc01d10c4187bc05ad611ba268ed02a96
Mho <ommtripathi046@gmail.com>
2025-12-23 12:42:40 +0530

    AlienDictionary

---
+++ Graphs/Leetcode/Traversal/minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -0,0 +1,27 @@
+// Topological sorting
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+
+        vector<vector<int>>adj(n);
+        vector<int>inDeg(n,0);
+        for(auto &edge:edges)
+        {
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back(v);
+            inDeg[v]++;
+        }
+        vector<int>result;
+
+        for(int i =0;i<n;i++)
+        {
+            if(inDeg[i]==0)
+            {
+                result.push_back(i);
+            }
+        }
+
+        return result;
+    }
+};
