83fc96d365a15aac21f6938a223d66c2a6524426
Mho <ommtripathi046@gmail.com>
2025-12-26 13:43:57 +0530

    validate-binary-tree-nodes

--- Graphs/Leetcode/DSU/validate-binary-tree-nodes.cpp
+++ Graphs/Leetcode/DSU/validate-binary-tree-nodes.cpp
@@ -1,3 +1,4 @@
+// using binary tree property and bfs 
 class Solution {
 public:
     bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
@@ -51,5 +52,87 @@ public:
         }
     
         return count == n;
+    }
+};
+
+// using dsu
+
+
+class Solution {
+public:
+    vector<int>parent;
+    vector<int>rank;
+    int find(int i)
+    {
+        if(i==parent[i])return i;
+
+        return parent[i] = find(parent[i]);
+    }
+
+    void Union(int x,int y)
+    {
+        int parx = find(x);
+        int pary = find(y);
+        if(parx==pary)
+        {
+            return ;
+        }
+        if(rank[parx]>rank[pary]){
+            parent[pary] = parx;
+        }else if(rank[pary]>rank[parx])
+        {
+            parent[parx] = pary;
+        }
+        else{
+            parent[parx] = pary;
+            rank[pary]++;
+        }
+    }
+    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
+
+        parent.resize(n);
+        rank.resize(n);
+        vector<int>inDeg(n,0);
+        for(int i=0;i<n;i++)
+        {
+            parent[i] = i;
+            rank[i]=0;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(leftChild[i]!=-1)
+            {
+                inDeg[leftChild[i]]++;
+                Union(i,leftChild[i]);
+                if(inDeg[leftChild[i]]>1){
+                    return false;
+                }
+            }
+            if(rightChild[i]!=-1)
+            {
+                inDeg[rightChild[i]]++;
+                Union(i,rightChild[i]);
+                if(inDeg[rightChild[i]]>1){
+                    return false;
+                }
+            } 
+        }
+        int root = -1;
+        for (int i = 0; i < n; i++) {
+            if (inDeg[i] == 0) {
+                if (root != -1) return false;
+                root = i;
+            }
+        }
+        if (root == -1) return false;
+
+        
+        int rootParent = find(root);
+        for (int i = 0; i < n; i++) {
+            if (find(i) != rootParent)
+                return false;
+        }
+
+        return true;
     }
 };
