7ce698674f9e4fcaa1b818585124569882fa9eb8
Mho <ommtripathi046@gmail.com>
2026-01-02 19:16:54 +0530

    minimum-number-of-operations-to-make-elements-in-array-distinct

---
+++ Easy/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -0,0 +1,50 @@
+// brute force
+class Solution {
+public:
+    bool isDistinct(int idx,vector<int>&nums){
+        unordered_set<int>st;
+        for(int i =idx;i<nums.size();i++)
+        {
+            if(st.find(nums[i])!=st.end()){
+                return false;
+            }
+            st.insert(nums[i]);
+        }
+        return true;
+    }
+    int minimumOperations(vector<int>& nums) {
+        int n = nums.size();
+        int count =0;
+        for(int i=0;i<n;i+=3)
+        {
+           if(isDistinct(i,nums))
+           {
+              break;
+           } 
+           count++;
+        }
+
+        return count;
+    }
+};
+
+// optimised sol
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int>st;
+        int idx =n;
+        for(int i=n-1;i>=0;i--)
+        {
+           if(st.count(nums[i]))
+           {
+              break;
+           } 
+           st.insert(nums[i]);
+           idx = i;
+        }
+
+        return ceil((idx+2)/3);
+    }
+};
