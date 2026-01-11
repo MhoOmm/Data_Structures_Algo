826218dd38752413cb6a6a86dc43a67c565d059c
Mho <ommtripathi046@gmail.com>
2026-01-12 00:45:26 +0530

    is-graph-bipartite

--- Graphs/Leetcode/Miscellaneous/is-graph-bipartite.cpp
+++ Graphs/Leetcode/Miscellaneous/is-graph-bipartite.cpp
@@ -38,3 +38,48 @@ public:
         return true;
     }
 };
+
+
+// bfs approach
+
+class Solution {
+public:
+    bool bfs(int i,vector<vector<int>>& adj,vector<int>&color,int currcolor)
+    {
+        queue<int>q;
+        q.push(i);
+        color[i] = currcolor;
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            for(auto &neig:adj[node]){
+                if(color[neig]==color[node])
+                {
+                    return false;
+                }
+                if(color[neig] == -1)
+                {
+                    color[neig] = 1-color[node];
+                    q.push(neig);
+                }
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int>color(n,-1);
+        int currcolor = 0;
+        for(int i =0;i<n;i++)
+        {
+            if(color[i]==-1)
+            {
+                if(bfs(i,graph,color,currcolor)==false){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
