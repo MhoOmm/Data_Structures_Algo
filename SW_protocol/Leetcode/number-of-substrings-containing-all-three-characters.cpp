3c6b4949605db884dfa3c3d235b5303b389e8513
Mho <ommtripathi046@gmail.com>
2026-01-21 20:31:46 +0530

    number-of-substrings-containing-all-three-characters

---
+++ SW_protocol/Leetcode/number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n = s.length();
+        unordered_map<char,int>mp;
+        int i =0;
+        int j =0;
+        int result =0;
+        while(j<n){
+            mp[s[j]] ++;
+            while(mp.size()==3){
+                result += n - j;
+                mp[s[i]] --;
+                if(mp[s[i]]==0)
+                {
+                    mp.erase(s[i]);
+                }
+                i++;
+            }
+            j++;
+        }
+
+        return result;
+    }
+};
