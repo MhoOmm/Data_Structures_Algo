5a6708ecc917a3d4e33de69e2a5e7a94db498681
Mho <ommtripathi046@gmail.com>
2026-01-29 22:49:04 +0530

    N_queens

--- Bactracking/N_queens.cpp
+++ Bactracking/N_queens.cpp
@@ -1,143 +1,95 @@
-#include<iostream>
-using namespace std;
-#include<vector>
-
-
 class Solution {
 public:
-
-    // to check both the diagonals
-    bool check(int n,vector<string>Board,int i,int j)
+    int n;
+    bool isValid(vector<string>&board,int row,int col)
     {
-        int row=i,col=j;
-        while(row>-1 && col>-1)
+        for(int i=row-1;i>=0;i--)
         {
-            if (Board[row][col]=='Q')
+            if(board[i][col]=='Q')
             {
-                return 0;
+                return false;
             }
-            row--;
-            col--;
+        }
+        for(int i = row, j = col; i>=0 && j >= 0; i--, j--) {
+            if(board[i][j] == 'Q')
+                return false;
+        }
+        for(int i = row, j = col; i>=0 && j<board.size(); i--, j++) {
+            if(board[i][j] == 'Q')
+                return false;
         }
 
-        row =i;
-        col=j;
-        while(row>-1 && col<n)
-        {
-            if (Board[row][col]=='Q')
-            {
-                return 0;
-            }
-            row--;
-            col++;
-        }
-        return 1;
-
+        return true;
     }
-    void nqueens(int row,int n,vector<vector<string>>&ans,vector<string>&Board,vector<bool>col)
-    {
-        //base case
-
-        if (row==n)
+    void solve(vector<string>&board,int row,vector<vector<string>>&result){
+        if(row>=n)
         {
-            ans.push_back(Board);
-            return;
+            result.push_back(board);
+            return ;
         }
 
-        // checking condition to enter queen
-        for (int j=0;j<n;j++)
+        for(int col=0;col<n;col++)
         {
-            if (col[j]==0 && check(n,Board,row,j))
+            if(isValid(board,row,col))
             {
-                col[j]=1;
-                Board[row][j]='Q';
-                nqueens(row+1,n,ans,Board,col);
-                col[j]=0;
-                Board[row][j]='.';
+                board[row][col] = 'Q';
+                solve(board,row+1,result);
+                board[row][col] = '.';
             }
         }
-    
     }
-    vector<vector<string>> solveNQueens(int n) {
-
-        vector<vector<string>>ans;
-        vector<string>Board(n);
-        for(int i =0 ;i<n ;i++)
-        {
-            for ( int j=0;j<n;j++)
-            {
-                Board[i].push_back('.');
-            }
-        }
-
-        vector<bool>col(n,0);
-
-        nqueens(0,n,ans,Board,col);
-
-        return ans ;
-
-        
-        
+    vector<vector<string>> solveNQueens(int N) {
+        n=N;
+        vector<string> board(n,string(n,'.'));
+        vector<vector<string>>result;
+        solve(board,0,result);
+        return result;
     }
 };
-
-
-
-
 // more optimized
-class Solution2 {
+class Solution {
 public:
+    int n;
 
-    
-    void nqueens(int row,int n,vector<vector<string>>&ans,vector<string>&Board,vector<bool>col,vector<bool>leftdig,vector<bool>rightdig)
-    {
-        //base case
+    void solve(vector<string>& board, int row, vector<vector<string>>& result,
+               unordered_set<int>& diag, unordered_set<int>& cols, unordered_set<int>& antiD) {
 
-        if (row==n)
-        {
-            ans.push_back(Board);
+        if(row >= n) {
+            result.push_back(board);
             return;
         }
 
-        // checking condition to enter queen
-        for (int j=0;j<n;j++)
-        {
-            if (col[j]==0 && rightdig[row+j]==0 && leftdig[(j-row)+(n-1)]==0 )
-            {
-                col[j]=1;
-                Board[row][j]='Q';
-                rightdig[row+j]=1;
-                leftdig[(j-row)+(n-1)]=1;
-                nqueens(row+1,n,ans,Board,col,leftdig,rightdig);
-                col[j]=0;
-                Board[row][j]='.';
-                rightdig[row+j]=0;
-                leftdig[(j-row)+(n-1)]=0;
-            }
+        for(int col = 0; col < n; col++) {
+            int d = row + col;   
+            int a = row - col;      
+
+            if(cols.count(col) || diag.count(d) || antiD.count(a))
+                continue;
+
+            board[row][col] = 'Q';
+            cols.insert(col);
+            diag.insert(d);
+            antiD.insert(a);
+
+            solve(board, row + 1, result, diag, cols, antiD);
+            board[row][col] = '.';
+            cols.erase(col);
+            diag.erase(d);
+            antiD.erase(a);
         }
-    
     }
-    vector<vector<string>> solveNQueens(int n) {
 
-        vector<vector<string>>ans;
-        vector<string>Board(n);
-        for(int i =0 ;i<n ;i++)
-        {
-            for ( int j=0;j<n;j++)
-            {
-                Board[i].push_back('.');
-            }
-        }
+    vector<vector<string>> solveNQueens(int N) {
+        n = N;
+        vector<string> board(n, string(n, '.'));
+        vector<vector<string>> result;
 
-        vector<bool>col(n,0);
-        vector<bool>leftdig(2*n-1,0);
-        vector<bool>rightdig(2*n-1,0);
+        unordered_set<int> diag;
+        unordered_set<int> antiD;
+        unordered_set<int> cols;
 
-        nqueens(0,n,ans,Board,col,leftdig,rightdig);
+        solve(board, 0, result, diag, cols, antiD);
 
-        return ans ;
-
-        
-        
+        return result;
     }
 };
