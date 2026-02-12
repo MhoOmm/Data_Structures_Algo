c93ede8def9ea8a0d23d716fc86848242604bd83
Mho <ommtripathi046@gmail.com>
2026-02-12 19:08:47 +0530

    my-calendar-ii

---
+++ LineSweepTechnique/my-calendar-ii.cpp
@@ -0,0 +1,31 @@
+// Using LineSweepTechinique
+class MyCalendarTwo {
+public:
+    map<int,int>events;    
+    MyCalendarTwo() {
+        
+    }
+    
+    bool book(int startTime, int endTime) {
+        events[startTime]++;
+        events[endTime]--;
+        int count =0;
+        for(auto &it:events)
+        {
+            count+=it.second;
+            if(count>2){
+                events[startTime]-=1;
+                events[endTime]+=1;
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(startTime,endTime);
+ */
