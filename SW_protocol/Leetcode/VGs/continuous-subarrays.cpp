8e53aaad92af4ce81e335f8e586af4eaa597e311
Mho <ommtripathi046@gmail.com>
2026-02-19 20:11:59 +0530

    continuous-subarrays

---
+++ SW_protocol/Leetcode/VGs/continuous-subarrays.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        long long result = 0;
+        int i =0;
+        int j =0;
+        map<int,int>mp;
+
+        while(j<n)
+        {
+            mp[nums[j]]++;
+            while(!(abs(mp.rbegin()->first - mp.begin()->first) <= 2))
+            {
+                mp[nums[i]]--;
+                if(mp[nums[i]]==0)
+                {
+                    mp.erase(nums[i]);
+                }
+                i++;
+            }
+            result += j-i+1;
+            j++;
+        }
+
+        return result;
+    }
+};
