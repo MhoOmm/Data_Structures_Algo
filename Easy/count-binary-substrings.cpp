801f62b9517e00e2ca7bad8a40a1d7aecc01c0b4
Mho <ommtripathi046@gmail.com>
2026-02-19 12:49:51 +0530

    count-binary-substrings

---
+++ Easy/count-binary-substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int n = s.length();
+        int prev = 0;
+        int curr = 1;
+        int result = 0;
+        for(int i=1;i<n;i++)
+        {
+            if(s[i]==s[i-1])
+            {
+                curr++;
+                continue;
+            }
+            else{
+                result += min(prev,curr); 
+                prev = curr;
+                curr = 1;  
+            }
+
+        }
+
+        return result + min(prev ,curr);
+    }
+};
