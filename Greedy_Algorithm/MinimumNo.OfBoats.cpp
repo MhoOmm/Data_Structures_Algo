ce75b59e7a16c81d4d41424e85ff8a6077321268
Mho <ommtripathi046@gmail.com>
2026-02-25 19:47:15 +0530

    boats-to-save-people

--- Greedy_Algorithm/MinimumNo.OfBoats.cpp
+++ Greedy_Algorithm/MinimumNo.OfBoats.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        int n=people.size();
+        int n = people.size();
+        sort(people.begin(),people.end());
         int i=0;
         int j=n-1;
-        int count=0;
-        sort(people.begin(),people.end());
+        int boats =0;
         while(i<=j)
         {
-            if(people[i]+people[j]>limit)
+            if(people[i]+people[j] > limit)
             {
+                boats++;
                 j--;
-                count++;
-            }
-            else if(people[i]+people[j]<=limit)
-            {
+            }else{
+                boats++;
                 i++;
                 j--;
-                count++;
             }
         }
-        return count;
+        return boats;
     }
 };
