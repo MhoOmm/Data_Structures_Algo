545d8ffc7369852d3451afd29d41d4668ff8f188
Mho <ommtripathi046@gmail.com>
2026-03-05 23:31:27 +0530

    minimum-changes-to-make-alternating-binary-string

--- Arrays/Leetcode/Array/minimum-changes-to-make-alternating-binary-string.cpp
+++ Arrays/Leetcode/Array/minimum-changes-to-make-alternating-binary-string.cpp
@@ -39,3 +39,35 @@ public:
         return min(countzero,countone);
     }
 };
+
+
+// Slightly better (jahan jahan hamein startsone mein edit karna hoga udhar starts
+// startszero mein update nhi karna hai toh )
+// countzero = n-countone;
+
+class Solution {
+public:
+    int minOperations(string s) {
+        int countzero = 0;
+        int countone = 0;
+        int n = s.length();
+
+        // starts with one
+        string temp = s;
+        if(s[0]!='1')
+        {
+            s[0] = '1';
+            countone++;
+        }
+        for(int i=1;i<n;i++)
+        {
+            if(s[i-1]==s[i])
+            {
+                countone++;
+                s[i] = s[i]=='1'?'0':'1';
+            }
+        }
+        countzero = n-countone;
+        return min(countzero,countone);
+    }
+};
