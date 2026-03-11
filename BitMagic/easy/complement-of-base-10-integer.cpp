aaab86d7d91322dfac97fa525aac6060a3e9b342
Mho <ommtripathi046@gmail.com>
2026-03-11 23:28:18 +0530

    Complement of base 10 integer

--- BitMagic/easy/complement-of-base-10-integer.cpp
+++ BitMagic/easy/complement-of-base-10-integer.cpp
@@ -1,3 +1,4 @@
+// Brute force approach
 class Solution {
 public:
     int bitwiseComplement(int n) {
@@ -26,6 +27,30 @@ public:
         }
         int ans = stoi(binary,nullptr,2);
         
+        return ans;
+        
+    }
+};
+
+// Smart approach
+
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int count =0;
+        int ans =0;
+        if(n==0)
+        {
+            return 1;
+        }
+        while(n>0)
+        {
+            int r = n%2;
+            ans += (pow(2,count) * !r);
+            n = n/2;
+            count++;
+        }
+
         return ans;
         
     }
