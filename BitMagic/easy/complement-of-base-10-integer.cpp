34deb48f1657fa76bc87484c9c09ac552b2171c3
Mho <ommtripathi046@gmail.com>
2026-03-11 23:18:23 +0530

    Number complement and complement-of-base-10-integer

---
+++ BitMagic/easy/complement-of-base-10-integer.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+
+        string binary = "";
+        if(n==0)
+        {
+            return 1;
+        }
+        while(n>0)
+        {
+            int r = n%2;
+            char ch = '0' + r;
+            binary += ch;
+            n = n/2;
+        }
+        reverse(begin(binary),end(binary));
+        for(auto &ch:binary)
+        {
+            if(ch=='0')
+            {
+                ch = '1';
+            }else{
+                ch= '0';
+            }
+        }
+        int ans = stoi(binary,nullptr,2);
+        
+        return ans;
+        
+    }
+};
