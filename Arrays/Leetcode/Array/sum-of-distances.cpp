c29f427cb8fc7b75ee1704cc3b6d3b08daa412b2
Mho <ommtripathi046@gmail.com>
2026-04-23 11:21:55 +0530

    sum-of-distances

--- Arrays/Leetcode/Array/sum-of-distances.cpp
+++ Arrays/Leetcode/Array/sum-of-distances.cpp
@@ -21,4 +21,37 @@ public:
     }
 };
 
-// Optimal Approach O()
+// Optimal Approach O(n) using map
+class Solution {
+public:
+    typedef long long ll;
+    vector<ll> distance(vector<int>& nums) {
+        int n = nums.size();
+        vector<ll>arr(n,0);
+        unordered_map<int,ll>indexSum;
+        unordered_map<int,ll>freqIndex;
+
+        // left pass
+        for(int i=0;i<n;i++)
+        {
+            ll numFreq = freqIndex[nums[i]];
+            ll sumi = indexSum[nums[i]]; 
+            arr[i] += (numFreq*i-sumi);
+            freqIndex[nums[i]]+=1;
+            indexSum[nums[i]]+=i;
+        }
+        freqIndex.clear();
+        indexSum.clear();
+        // right pass
+        for(int i=n-1;i>=0;i--)
+        {
+            ll numFreq = freqIndex[nums[i]];
+            ll sumi = indexSum[nums[i]]; 
+            arr[i] += (sumi - numFreq*i);
+            freqIndex[nums[i]]+=1;
+            indexSum[nums[i]]+=i;
+        }
+        return arr;
+    }
+};
+
