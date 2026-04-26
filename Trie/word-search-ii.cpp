efa5f29bb6f275b0576515df52721f020fb3b996
Mho <ommtripathi046@gmail.com>
2026-04-26 23:51:36 +0530

    word-search-ii

--- Trie/word-search-ii.cpp
+++ Trie/word-search-ii.cpp
@@ -1,79 +1,86 @@
 class Solution {
-public: 
-    vector<vector<int>>directions{{0,1},{1,0},{-1,0},{0,-1}};
+public:
     int m,n;
-    vector<string>result;
-    struct trie{
+    vector<string>ans;
+    struct trieNode{
         bool eow;
-        trie* child[26];
+        trieNode* child[26];
         string word;
     };
-    trie* getNode(){
-        trie* newn = new trie();
-        newn->eow = false;
-        newn->word="";
+    vector<vector<int>>directions{{0,1},{0,-1},{1,0},{-1,0}};
+    trieNode* getNode()
+    {
+        trieNode* node = new trieNode();
+        node->word = "";
+        node->eow = false;
         for(int i=0;i<26;i++)
         {
-            newn->child[i] = NULL;
+            node->child[i] = NULL;
         }
-        return newn;
+        return node; 
     }
-    void insert(trie* root,string input){
-        trie* crawl = root;
-        for(int i=0;i<input.length();i++)
+    trieNode* root ;
+    void insert(string word)
+    {
+        trieNode* crawl = root;
+        for(auto &ch:word)
         {
-            int idx = input[i]-'a';
-            if(crawl->child[idx] == NULL){
+            int idx = ch-'a';
+            if(crawl->child[idx]==NULL)
+            {
                 crawl->child[idx] = getNode();
             }
-            crawl  = crawl->child[idx];
+            crawl = crawl->child[idx];
         }
         crawl->eow = true;
-        crawl->word = input;
+        crawl->word = word;
     }
-
-    void dfs(int i,int j,trie* root,vector<vector<char>>& board)
+    void dfs(trieNode* root,int i,int j,vector<vector<char>>&board)
     {
-        if(i>=m||j>=n||i<0||j<0||board[i][j]=='$'||root->child[board[i][j]-'a'] == NULL ){
+        if(i>=m || j>=n || i<0 || j<0 ||board[i][j]=='$')
+        {
             return;
         }
-        root = root->child[board[i][j] - 'a'];
-        if(root->eow == true)
+        char ch = board[i][j];
+        if(root->child[ch-'a']==NULL)
         {
-            result.push_back(root->word);
-            root->eow = false; // to prevent duplicate entry in the result array
+            return;
+        }
+        root = root->child[ch-'a'];
+        if(root->eow==true)
+        {
+            string w = root->word;
+            ans.push_back(w);
+            root->eow = false;
         }
         char temp = board[i][j];
         board[i][j] = '$';
-        for(auto &dir:directions)
-        {
-            int ni = i+ dir[0];
-            int nj = j+ dir[1];
-            dfs(ni,nj,root,board);
-        } 
-        board[i][j]= temp;
+        for(auto &dir:directions){
+            int ni = i + dir[0];
+            int nj = j + dir[1];
+            dfs(root,ni,nj,board);
+        }
+        board[i][j] = temp;
     }
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
         m = board.size();
         n = board[0].size();
-        trie* root = getNode();
-
-        for(int i=0;i<words.size();i++)
+        root = getNode();
+        for(auto &word:words)
         {
-            insert(root,words[i]);
+            insert(word);
         }
-        for(int i =0;i<m;i++)
+        for(int i=0;i<m;i++)
         {
-            for(int j =0;j<n;j++)
+            for(int j=0;j<n;j++)
             {
                 char ch = board[i][j];
-                if(root->child[ch-'a'] != NULL)
+                if(root->child[ch-'a']!=NULL)
                 {
-                    dfs(i,j,root,board);
+                    dfs(root,i,j,board);
                 }
             }
         }
-
-        return result;
+        return ans;
     }
 };
