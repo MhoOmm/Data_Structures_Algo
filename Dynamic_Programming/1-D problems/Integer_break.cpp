8f80ae898794d8a38b51d82c45f93d0d010c3c09
Mho <ommtripathi046@gmail.com>
2026-05-08 08:32:46 +0530

    Integer Break

--- Dynamic_Programming/1-D problems/Integer_break.cpp
+++ Dynamic_Programming/1-D problems/Integer_break.cpp
@@ -1,3 +1,4 @@
+// backward pass (n->1)
 class Solution {
 public:
     int t[59];
@@ -21,5 +22,41 @@ public:
     int integerBreak(int n) {
         memset(t,-1,sizeof(t));
         return solve(n);
+    }
+};
+
+// forward pass solution (0->n)
+
+class Solution {
+public:
+    int t[60];
+    int solve(int sum,int n)
+    {
+        if(sum==n)
+        {
+            return 1;
+        }
+        if(sum>n)
+        {
+            return 0;
+        }
+        if(t[sum]!=-1)
+        {
+            return t[sum];
+        }
+        int ans = INT_MIN;
+        for(int i=1;i<n;i++)
+        {
+            int child = solve(sum+i,n);
+            if(child>0)
+            {
+                ans = max(ans,i*child);
+            }
+        }
+        return t[sum] = ans;
+    }
+    int integerBreak(int n) {
+        memset(t,-1,sizeof(t));
+        return solve(0,n);
     }
 };
