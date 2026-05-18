2dacdf55363386efa7c6b98197b06810557e0a60
Mho <ommtripathi046@gmail.com>
2026-05-18 11:59:55 +0530

    Solving questions with brain power

--- Dynamic_Programming/1-D problems/SolveQuestionsWithBrainpower.cpp
+++ Dynamic_Programming/1-D problems/SolveQuestionsWithBrainpower.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
     int n;
     long long t[100001];
-    long long solve(int i,vector<vector<int>>& questions)
+    long long solve(int i,vector<vector<int>>& qn)
     {
         if(i>=n)
         {
@@ -13,36 +13,37 @@ public:
         {
             return t[i];
         }
-        int cost = questions[i][0];
-        long dist = questions[i][1];
-        long long take = cost + solve(i+dist+1,questions);
-        long long skip = solve(i+1,questions);
+        // take
+        long long take = qn[i][0] + solve(i+qn[i][1]+1,qn);
+        // skip
+        long long skip = solve(i+1,qn);
 
         return t[i] = max(take,skip);
     }
     long long mostPoints(vector<vector<int>>& questions) {
         n = questions.size();
         memset(t,-1,sizeof(t));
-        return solve(0,questions) ; 
+        return solve(0,questions);
     }
 };
-
 
 // Bottom Up Approach (O(N^2)) (Good Bottom Up) (right to left)
 
 class Solution {
 public:
-    long long mostPoints(vector<vector<int>>& q) {
-        int n = q.size();
-
-        vector<long long >t(200001,0);
-
+    long long mostPoints(vector<vector<int>>& qn) {
+        int n = qn.size();
+        vector<long long>t(n+1,0);
         for(int i=n-1;i>=0;i--)
         {
-            t[i] = max(q[i][0]+t[i + q[i][1] + 1],t[i+1]);
+            long long add = (i+qn[i][1]+1)<n?t[i+qn[i][1]+1]:0;
+            // take
+            long long take = qn[i][0]+add;
+            // skip
+            long long skip = t[i+1];
+            t[i] = max(take,skip);
         }
 
         return t[0];
-        
     }
 };
