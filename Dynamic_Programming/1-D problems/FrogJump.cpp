f6085070b03123a2b4e7cf3554a23b9abdd65e0d
Mho <ommtripathi046@gmail.com>
2026-06-05 10:51:39 +0530

    fropg jump

--- Dynamic_Programming/1-D problems/FrogJump.cpp
+++ Dynamic_Programming/1-D problems/FrogJump.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
-    int n ;
+    int last;
     unordered_map<int,int>mp;
-
     int t[2001][2001];
-
-    bool solve(int csi,int pj,vector<int>& stones)
+    bool solve(int csi,int pj,vector<int>&stones)
     {
-        if(csi==n-1)
+        if(csi==(stones.size()-1))
         {
             return true;
         }
-
         if(t[csi][pj]!=-1)
         {
             return t[csi][pj];
         }
-
         bool result = false;
-
-        for(int idx = pj-1;idx<=pj+1;idx++)
+        // if im at currStone and have prevJump i can go 
+        for(int i=(pj-1);i<=(pj+1);i++)
         {
-            if(idx>0)
+            if(i>0)
             {
-                int nextst = stones[csi]+idx;
-                if(mp.find(nextst)!=mp.end()){
-                    result = result || solve(mp[nextst],idx,stones);
+                int nj = stones[csi]+i;
+                if(mp.find(nj)!=mp.end())
+                {
+                    result = result || solve(mp[nj],i,stones);
                 }
             }
         }
-        return t[csi][pj]=result;
+
+        return t[csi][pj] = result;
     }
     bool canCross(vector<int>& stones) {
-        n = stones.size();
-
-        if(stones[1]!=1)return false;
-        for(int i = 0;i<n;i++)
-        {
-            mp[stones[i]]=i;
-        }
+        int n = stones.size();
         memset(t,-1,sizeof(t));
-        return solve(0,0,stones);
+        for(int i =0;i<n;i++)
+        {
+            mp[stones[i]] = i;
+        }
+        if(stones[1]!=1)return false;
+        return solve(1,1,stones);
     }
 };
