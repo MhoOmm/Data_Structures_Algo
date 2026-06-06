dcc7014174c51c2fe8124b3d8c440f802a4ab0d7
Mho <ommtripathi046@gmail.com>
2026-06-06 19:46:38 +0530

    left-and-right-sum-differences

---
+++ Easy/left-and-right-sum-differences.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>leftSum(n,0);
+        vector<int>rightSum(n,0);
+        
+        for(int i=1;i<n;i++)
+        {
+            leftSum[i] = nums[i-1]+leftSum[i-1];
+        }
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            rightSum[i] = nums[i+1]+rightSum[i+1];
+        }
+
+        // answer array
+        vector<int>ans(n,0);
+        for(int i=0;i<n;i++)
+        {
+            ans[i] = abs(leftSum[i]-rightSum[i]);
+        }
+
+        return ans;
+    }
+};
