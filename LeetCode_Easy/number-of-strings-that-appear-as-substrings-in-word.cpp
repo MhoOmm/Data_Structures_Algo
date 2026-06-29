3bbf9eeaa00f7023e3a8ab39e12f7ccbf15e77a7
Mho <ommtripathi046@gmail.com>
2026-06-29 20:36:22 +0530

    potd

---
+++ LeetCode_Easy/number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int count =0;
+        for(auto &str:patterns)
+        {
+            if(word.find(str)!= string::npos)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
