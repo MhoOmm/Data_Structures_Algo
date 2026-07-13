bb9d5151c4ac565b422ac1639c65e3ea9934a215
Mho <ommtripathi046@gmail.com>
2026-07-13 22:45:33 +0530

    find-minimum-time-to-reach-last-room djikstra

--- Graphs/Leetcode/Djikstra/find-minimum-time-to-reach-last-room-i.cpp
+++ Graphs/Leetcode/Djikstra/find-minimum-time-to-reach-last-room-i.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
     typedef pair<int,pair<int,int>> pp;
-    vector<vector<int>>directions{{0,1},{1,0},{-1,0},{0,-1}};
-    int minTimeToReach(vector<vector<int>>& moveTime) {
-        int n = moveTime.size();
-        int m = moveTime[0].size();
-        vector<vector<int>>result(n,vector<int>(m,INT_MAX));
-        priority_queue<pp,vector<pp>,greater<pp>> pq;
-        result[0][0] =  0;
+    vector<vector<int>>directions{{0,1},{1,0},{0,-1},{-1,0}};
+    int minTimeToReach(vector<vector<int>>& mt) {
+        int m = mt.size();
+        int n = mt[0].size();
+
+        priority_queue<pp,vector<pp>,greater<pp>>pq;
+        vector<vector<int>> result(m, vector<int>(n, INT_MAX));
         pq.push({0,{0,0}});
+        result[0][0] = 0;
+        int cross = 1;
+
         while(!pq.empty()){
             int currTime = pq.top().first;
             int i = pq.top().second.first;
             int j = pq.top().second.second;
-
             pq.pop();
-            if(i==n-1 && j==m-1)
+
+            if(i==m-1 && j==n-1)
             {
                 return currTime;
             }
-            for(auto &dir:directions){
-                int ni = i + dir[0];
-                int nj = j + dir[1];
-                if(ni>=0 && nj>=0 && ni<n && nj<m)
+
+            for(auto &dir:directions)
+            {
+                int ni = i+dir[0];
+                int nj = j+dir[1];
+                if(ni>=0 && nj>=0 && ni<m && nj<n)
                 {
-                    int wait = max(moveTime[ni][nj]-currTime , 0);
-                    int nt = wait + currTime + 1;
-                    if (result[ni][nj]> nt)
+                    int wait = max(mt[ni][nj] - currTime , 0 );
+                    int time = wait + currTime + cross;
+                    if(result[ni][nj]>time)
                     {
-                        result[ni][nj] = nt;
-                        pq.push({nt,{ni,nj}});
+                        result[ni][nj]=time;
+                        pq.push({time,{ni,nj}}); 
                     }
                 }
             }
