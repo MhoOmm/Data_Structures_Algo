8f4b543491c0f37e573cb3cc61bdc40197573110
Mho <ommtripathi046@gmail.com>
2026-07-19 19:37:31 +0530

    potd

---
+++ Stack/Leetcode/MonotonicStack/smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        string result = "";
+        vector<int>lastIdx(26,0);
+        vector<int>visited(26,false);
+
+        for(int i=0;i<s.length();i++)
+        {
+            lastIdx[s[i]-'a'] = i;
+        }
+
+        for(int i=0;i<s.length();i++)
+        {
+            char ch = s[i];
+            int idx = s[i]-'a';
+            if(visited[idx]){
+                continue;
+            }
+
+            while(result.length()>0 && result.back() > ch && lastIdx[result.back()-'a']>i)
+            {
+                visited[result.back()-'a'] = false;
+                result.pop_back();
+            }
+
+            result.push_back(ch);
+            visited[idx] = true;
+        }
+        return result;
+    }
+};
